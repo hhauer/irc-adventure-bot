206e5ec3aa4ce5472e99e0d00a8366b12c8c9565
Harley Hauer <harley.hauer@gmail.com>
2014-04-02 14:54:19 -0700

    Added logging to help introspect energy values during development.

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
-
+import json
+import cPickle
+import copy
 
 # Logging
 import logging
@@ -19,32 +21,37 @@ class Engine(object):
         return score
 
     def token_value(self, token):
-        energy = 25
+        energy = 10
 
         if token not in self.words:
             self.words[token] = {
-                    'energy': energy,
                     'last_used': datetime.now(),
+                    'times': 1,
                     }
         else:
             scale = self.energy_scale(self.words[token]['last_used'], datetime.now())
-            energy = self.words[token]['energy'] * scale
+            energy *= scale
+            self.words[token]['times'] += 1
+            logger.debug("[{}] Scale: {} | Energy: {}".format(token, scale, energy))
 
         if energy < 1:
             energy = 1
 
-        self.words[token]['energy'] = energy
         return energy
 
     def energy_scale(self, last, current):
         delta = current - last
 
         # 60s * 60m = 1hr
-        scale = delta.total_seconds() / (60 * 60)
+        hours = delta.total_seconds() / (60 * 60)
 
-        if scale > 12:
-            return 12
-        elif scale < 0.05:
+        # Magic numbers from a regression analysis based on:
+        # fit {{0, 0}, {1, 0.2}, {2, 0.5}, {3, 0.9}, {24, 10}}
+        scale = 0.425122 * hours - 0.230732
+
+        if scale > 10:
+            return 10
+        elif scale < 0.1:
             return 0.1
         else:
             return scale

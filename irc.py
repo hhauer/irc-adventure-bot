c0a95eb8ae397ef08956b62c5c657aae2803510e
Harley Hauer <hhauer@pdx.edu>
2014-04-02 18:00:20 -0700

    Fleshing out of Listener functionality. Stubbed in necessary parts to allow clean disconnect command.

--- irc.py
+++ irc.py
@@ -1,5 +1,9 @@
+# Basic form largely from:
+# http://twistedmatrix.com/documents/13.2.0/words/examples/ircLogBot.py
+
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol
+from twisted.internet.error import ConnectionDone
 
 import enchant
 from enchant.tokenize import get_tokenizer
@@ -34,17 +38,30 @@ class Listener(irc.IRCClient):
     input_channels = ['#informationsociety']
     engine = Engine()
 
+    def __init__(self):
+        self.users = {}
+
     def connectionMade(self):
         irc.IRCClient.connectionMade(self)
+        logger.info("Connection established.")
 
-        # Join listen channels.
+    def connectionLost(self, reason):
+        irc.IRCClient.connectionLost(self, reason)
+        logger.info("Connection lost.")
+
+    # Event callbacks.
+    def signedOn(self):
+       # Join listen channels.
         for x in self.input_channels:
+            logger.debug("Joining listen channel %s.", x)
             self.join(x)
 
         # join output channel.
+        logger.debug("Joining output channel %s.", self.output_channel)
         self.join(self.output_channel)
 
-        self.users = {}
+    def joined(self, channel):
+        logger.info("Joined channel: %s.", channel)
 
     def privmsg(self, user, channel, message):
         user = user.split('!', 1)[0]
@@ -57,8 +74,19 @@ class Listener(irc.IRCClient):
             self.msg(self.output_channel, "{} -- [{}] Line: {} | Total: {}".format(channel, user, energy, self.users[user].energy))
         elif channel == self.nickname:
             self.msg(self.output_channel, "Received private message from {}".format(user))
-        else:
-            self.msg(self.output_channel, "Input from invalid source.")
+        #else:
+            #self.msg(self.output_channel, "Stopping reactor.")
+            #self.quit("Attempting graceful quit.")
+
+    def action(self, user, channel, message):
+        yield
+
+    # IRC Callbacks
+    def irc_NICK(self, prefix, params):
+        """Called when an IRC user changes their nickname."""
+        #old_nick = prefix.split('!')[0]
+        #new_nick = params[0]
+        #self.logger.log("%s is now known as %s" % (old_nick, new_nick))
 
 
 class ListenerFactory(protocol.ClientFactory):
@@ -66,7 +94,13 @@ class ListenerFactory(protocol.ClientFactory):
         return Listener()
 
     def clientConnectionLost(self, connector, reason):
-        connector.connect()
+        r = reason.check(ConnectionDone)
+        if r is ConnectionDone:
+            print "Clean disconnect."
+            reactor.stop()
+        else:
+            print "connection lost: ", reason
+            connector.connect()
 
     def clientConnectionFailed(self, connector, reason):
         print "connection failed: ", reason
